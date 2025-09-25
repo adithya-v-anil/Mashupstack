1f44f2a2fa02c4886fe7ea87ec232030afa7988a
adithya-anilkumar <adithyavanil7@gmail.com>
2025-09-25 15:47:35 +0530

    File System

---
+++ Main.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class Main {
+    public static void main(String[] args) {
+         File file = new File("assignment.txt");
+         String content = "Java File Handling Practice";
+
+     try {
+
+      boolean value = file.createNewFile();
+      if (value) {
+        System.out.println("The new file is created.");
+      }
+      else {
+        System.out.println("The file already exists.");
+      }
+
+      FileWriter output = new FileWriter(file);
+            output.write(content);
+            System.out.println("Data is written to the file.");
+            output.close();
+       
+      FileReader input = new FileReader(file);
+             int i;
+             System.out.println("Data in the file:");
+             while ((i = input.read()) != -1) {
+                System.out.print((char) i);
+            }
+            System.out.println();
+            input.close();
+
+    if (file.delete()) {
+                System.out.println("File deleted successfully: " + file.getName());
+            } else {
+                System.out.println("Failed to delete the file.");
+            }
+        }
+
+     catch(Exception e) {
+       System.out.println("An error occurred: " + e.getMessage());
+    }
+
+}
+}
