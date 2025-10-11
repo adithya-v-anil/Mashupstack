aa7af73e142d5d3d5d3dc6c2b29ed2d73a70a9af
adithya-anilkumar <adithyavanil7@gmail.com>
2025-10-12 00:18:35 +0530

    Print an Integer

---
+++ PrintInteger.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PrintInteger {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        System.out.println("You entered: " +number);
+        sc.close();
+    }
+}
