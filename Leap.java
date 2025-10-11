bc0b7b2544a07d8b76579076f49da8aa70f6cc83
adithya-anilkumar <adithyavanil7@gmail.com>
2025-10-12 00:12:34 +0530

    Check Leap Year

---
+++ Leap.java
@@ -0,0 +1,11 @@
+public class Leap {
+    public static void main(String args[]) {
+        int year = 2012;
+        if (year%4 == 0) {
+            System.out.println(year+" is a leap year");
+        }
+        else{
+            System.out.println(year+" is not a leap year");
+        }
+    }
+}
