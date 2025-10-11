ae361d7c4906271fce38889354a1acf8fa77a1dc
adithya-anilkumar <adithyavanil7@gmail.com>
2025-10-12 00:20:12 +0530

    Multiply Two Floating Point Numbers

---
+++ MultiplyNum.java
@@ -0,0 +1,8 @@
+public class MultiplyNum {
+    public static void main(String args[]){
+        float first = 1.5f;
+        float second = 2.5f;
+        float product = first * second;
+        System.out.print(first+ " * " +second+ " = " +product);
+    }
+}
