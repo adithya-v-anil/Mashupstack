7a915fd242096c873f65bca4d90494df6532f77f
adithya-anilkumar <adithyavanil7@gmail.com>
2025-10-12 00:03:49 +0530

    Multiply

--- MultiplyNum.java
+++
@@ -1,8 +0,0 @@
-public class MultiplyNum {
-    public static void main(String args[]){
-        float first = 1.5f;
-        float second = 2.5f;
-        float product = first * second;
-        System.out.print(first+ " * " +second+ " = " +product);
-    }
-}
