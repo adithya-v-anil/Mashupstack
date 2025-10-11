c49448a1027b2b828f1dbb0736b43397ac9f1c51
adithya-anilkumar <adithyavanil7@gmail.com>
2025-10-11 23:55:24 +0530

    Ascii

--- AsciiValue.java
+++
@@ -1,7 +0,0 @@
-public class AsciiValue {
-    public static void main(String args[]) {
-        char ch = 'a';
-        int Ascii = (int) ch ;
-        System.out.print("The ASCII value of " +ch+ " is " +Ascii);
-    }
-}
