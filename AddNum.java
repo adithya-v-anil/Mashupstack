93472bd241f233c74da4de40a1a7d41b3d981f61
adithya-anilkumar <adithyavanil7@gmail.com>
2025-10-11 23:54:35 +0530

    Add

--- AddNum.java
+++
@@ -1,8 +0,0 @@
-public class AddNum {
-    public static void main(String args[]){
-        int first = 10;
-        int second = 20;
-        int sum = first + second ;
-        System.out.print(first+ " + " +second+ " = " +sum);
-    }
-}
