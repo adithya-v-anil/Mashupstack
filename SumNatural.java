5d26d4f34ba1eb130b131cb3996d1e9e6ddedd50
adithya-anilkumar <adithyavanil7@gmail.com>
2025-10-12 00:18:51 +0530

    Sum of Natural Numbers

---
+++ SumNatural.java
@@ -0,0 +1,10 @@
+public class SumNatural {
+    public static void main(String args[]){
+        int number = 100;
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum+= i;
+        }
+        System.out.println("Sum of natural number: " +sum);
+    }
+}
